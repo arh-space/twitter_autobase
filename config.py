b79030173bd3de7c1a499e0f96c491c8074b3280
ARH <65483182+arh-space@users.noreply.github.com>
2021-10-08 10:34:19 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-consumer_key = "YOUR API KEY HERE"
-consumer_secret = "YOUR API KEY SECRET HERE"
-access_token = "YOUR ACCESS TOKEN HERE"
-access_token_secret = "YOUR ACCESS TOKEN SECRET HERE"
-trigger = "triggerword"
+consumer_key = "uDrDG2SK8erM7uj0X9qfiKDTm"
+consumer_secret = "EI21hKLXT3aDYfKlE6ICvwxrvD5Hf1td9bOXn87BH4DRvy6tLc"
+access_token = "1446167755703525387-o7nJgxOiFYJCFlGFeuUyw3Lsztgcnb"
+access_token_secret = "IgFepoQrjde4L8usl9MzTgqayTJ2d68e49o0B8CYebDOK"
+trigger = "TDB"
 timezone = 7 #change this into your timezone. for example if your timezone is GMT+7 you put 7
